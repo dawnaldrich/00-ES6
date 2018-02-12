1af8b6f81626869c2e996bfab0c08270bf5dfbde
Dawn Aldrich <dawnstarr.aldrich@gmail.com>
2018-02-12 11:03:42 -0800

    updated let to const

--- dawn-aldrich/js/app.js
+++ dawn-aldrich/js/app.js
@@ -1,22 +1,23 @@
 'use strict';
 
-let names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+const names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-let allProducts = [];
-let container = document.getElementById('image_container');
-let viewed = [];
-let labels = [];
-let pics = [document.getElementById('left'),
+const allProducts = [];
+const container = document.getElementById('image_container');
+const viewed = [];
+const labels = [];
+const pics = [document.getElementById('left'),
                 document.getElementById('center'), //eslint-disable-line
                 document.getElementById('right')]; //eslint-disable-line
-let list = document.getElementById('productlist');
-let totalClicks = 0;
-let views = [];
-let votes = [];
+const list = document.getElementById('productlist');
+const totalClicks = 0;
+const views = [];
+const votes = [];
 
 function Product(name) {
   this.name = name;
   this.path = 'img/' + name + '.jpg';
+ // this.path = `img/{$name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -29,19 +30,19 @@ function makeRandom() {
 function displayPics(){
   // roll for three random indexes
   while(viewed.length < 6){
-    let rando = makeRandom();
+    const rando = makeRandom();
     while(!viewed.includes(rando)){
       viewed.push(rando);
     }
   }
   //console.log(rando);
-  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the letiable declaration from `let to `let`.
+  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
   // rando is unknown or undefined outside of the codeblock it was declared
   console.log(viewed)
 
   // To the DOM and beyond!
   for (let i = 0; i < 3; i++){
-    let temp = viewed.shift();
+    const temp = viewed.shift();
     pics[i].src = allProducts[temp].path;
     pics[i].id = allProducts[temp].name;
     allProducts[temp].views += 1;
@@ -59,10 +60,11 @@ function handleClick(event) {
     return alert('Be sure to click directly on an image!!');
   }
   totalClicks += 1;
-  for(let i = 0; i < names.length; i++){
+  for(const i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
       console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+     // console.log(`${event.target.id} has ${allProducts[i].votes}  votes in  ${allProducts[i].views}  views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -71,9 +73,10 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(let i = 0; i < allProducts.length; i++) {
-    let liEl = document.createElement('li');
+  for(const i = 0; i < allProducts.length; i++) {
+    const liEl = document.createElement('li');
     liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+   // liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes}  votes in  ${allProducts[i].views}  views`;
     list.appendChild(liEl);
   }
 }
@@ -88,7 +91,7 @@ function makeChartData(){
 
 function makeChart(){
   makeChartData();
-  let ctx = document.getElementById('chartypants').getContext('2d');
+  const ctx = document.getElementById('chartypants').getContext('2d');
   new Chart(ctx, {
     type: 'bar',
     data: {
