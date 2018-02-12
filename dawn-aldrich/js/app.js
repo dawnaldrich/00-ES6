f1b3749b7e1381fc39362299e3f1e8f617eb2892
Dawn Aldrich <dawnstarr.aldrich@gmail.com>
2018-02-12 11:20:54 -0800

    converted template literate and fixed a few more errors using const

--- dawn-aldrich/js/app.js
+++ dawn-aldrich/js/app.js
@@ -2,7 +2,7 @@
 
 const names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-const allProducts = [];
+let allProducts = [];
 const container = document.getElementById('image_container');
 const viewed = [];
 const labels = [];
@@ -10,14 +10,15 @@ const pics = [document.getElementById('left'),
                 document.getElementById('center'), //eslint-disable-line
                 document.getElementById('right')]; //eslint-disable-line
 const list = document.getElementById('productlist');
-const totalClicks = 0;
+let totalClicks = 0;
 const views = [];
 const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
- // this.path = `img/{$name}.jpg`;
+  //this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg`;
+  console.log(this.path);
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -60,11 +61,11 @@ function handleClick(event) {
     return alert('Be sure to click directly on an image!!');
   }
   totalClicks += 1;
-  for(const i = 0; i < names.length; i++){
+  for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
-     // console.log(`${event.target.id} has ${allProducts[i].votes}  votes in  ${allProducts[i].views}  views`);
+      //console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id} has ${allProducts[i].votes}  votes in  ${allProducts[i].views}  views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -75,8 +76,8 @@ function handleClick(event) {
 function showList() {
   for(const i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
-   // liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes}  votes in  ${allProducts[i].views}  views`;
+   // liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes}  votes in  ${allProducts[i].views}  views`;
     list.appendChild(liEl);
   }
 }
